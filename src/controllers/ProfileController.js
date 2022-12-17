dd5087b56d1799d4cf4be51f700d02f9ef98f30e
“[MR <mizanurrahmanhridu@gmail.com>
2022-12-17 22:44:48 +0600

    new

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -1,4 +1,5 @@
 const ProfileModel = require('../models/ProfileModel');
+var jwt = require('jsonwebtoken');
 
 exports.CreateProfile=(req,res)=>{
     let reqBody=req.body;
@@ -11,8 +12,53 @@ exports.CreateProfile=(req,res)=>{
         }
     })
 }
-// exports.CreateProfile=(req,res)=>{
-//
-//             res.status(200).json({status:"success"})
-//
-// }
+
+
+exports.UserLogin=(req,res)=>{
+    let UserName = req.body['UserName'];
+    let Password = req.body['Password'];
+    ProfileModel.find({UserName: UserName, Password: Password},(err,data)=>{
+        if(err){
+            res.status(400).json({status:"Failed"})
+        }
+        else
+        {
+            if(data.length > 0){
+
+                // Creating auth token
+                let Payload={exp: Math.floor(Date.now() / 1000) + (24*60*60), data:data[0]}
+                let token = jwt.sign( Payload,'SecretKey');
+
+                res.status(200).json({status:"success",token:token,data:data[0]})
+            }
+            else{
+                res.status(400).json({status:"Unauthorized"})
+            }
+        }
+    })
+}
+
+exports.SelectProfile=(req,res)=>{
+    let UserName=req.headers['username']
+    ProfileModel.find({UserName:UserName},(err,data)=>{
+        if(err){
+            res.status(400).json({status:"fail",data:err})
+        }
+        else {
+            res.status(200).json({status:"success",data:data})
+        }
+    })
+}
+
+exports.UpdateProfile=(req,res)=>{
+    let UserName=req.headers['username']
+    let reqBody=req.body;
+    ProfileModel.updateOne({UserName:UserName},{$set:reqBody},{upsert:true},(err,data)=>{
+        if(err){
+            res.status(400).json({status:"fail",data:err})
+        }
+        else {
+            res.status(200).json({status:"success",data:data})
+        }
+    })
+}
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const ProfileController = require("../controllers/ProfileController");
+const ToDoListController = require("../controllers/ToDoListController")
+const AuthVerifyMiddleware = require("../middleware/AuthVerifyMiddleware")
 
 router.post("/CreateProfile",ProfileController.CreateProfile);
-// router.post("/UserLogin",ProfileController.UserLogin)
+router.post("/UserLogin",ProfileController.UserLogin)
+router.get("/SelectProfile",AuthVerifyMiddleware,ProfileController.SelectProfile)
+router.post("/UpdateProfile",AuthVerifyMiddleware,ProfileController.UpdateProfile)
+
+
+router.post("/CreateToDo",AuthVerifyMiddleware,ToDoListController.CreateToDo);
+router.get("/SelectToDo",AuthVerifyMiddleware,ToDoListController.SelectToDo);
+router.get("/UpdateToDo",AuthVerifyMiddleware,ToDoListController.UpdateToDo);
+
 module.exports = router;
 
 
